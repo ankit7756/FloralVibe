960f4d75255995506aa8fffe7737b608b5d98c1f
Pabitrakhadka517 <pratimakhadka517@gmail.com>
2024-12-22 08:40:07 +0545

    Index file added

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglePassword(id) {
+    const passwordField = document.getElementById(id);
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+  }
+  
